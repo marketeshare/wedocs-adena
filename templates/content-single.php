fc186f01972d27d2dcbc970d3049c27813f9673a
PH Munhoz <ph@marketeshare.com.br>
2016-03-11 11:30:13 -0300

    Inicia repositório da Adena

---
+++ templates/content-single.php
@@ -0,0 +1,36 @@
+<?php while (have_posts()) : the_post(); ?>
+    <?php $skip_sidebar = get_post_meta( $post->ID, 'skip_sidebar', true ); ?>
+
+    <article <?php post_class( 'row' ); ?>>
+
+        <div class="col-md-12">
+            <div class="entry-content">
+                <header>
+                    <h1 class="entry-title"><?php the_title(); ?></h1>
+                </header>
+
+                <?php the_content(); ?>
+            </div>
+
+            <?php
+
+            if ( $skip_sidebar != 'yes' ) {
+
+                $children = wp_list_pages("title_li=&order=menu_order&child_of=". $post->ID ."&echo=0&post_type=" . $post->post_type);
+
+                if ( $children ) {
+                    echo '<div class="article-child well">';
+                    echo '<h3>' . __( 'Articles', 'wedocs' ) . '</h3>';
+                    echo '<ul>';
+                    echo $children;
+                    echo '</ul>';
+                    echo '</div>';
+                }
+
+                get_template_part( 'templates/content', 'feedback' );
+            }
+            ?>
+
+        </div><!-- .col-md-# -->
+    </article>
+<?php endwhile; ?>
